5d916aa6d6296066dc62cf759d247ce63d105238
Sayalikanade <sayalikanade97@gmail.com>
2022-05-26 15:19:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [Hori, setHori] = useState(10)
+  const [Veri, setVeri] = useState(10)
+  const [Blur, setBlur] = useState(10)
+  const [Color, setColor] = useState("black")
+  const [CheckOn, setCheckon] = useState(false)
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Controls">
+        <label>Horizontal Length</label>
+        <input type="range" min="-200" max="200" value={Hori} onChange={(e) => setHori(e.target.value)} />
+        <label>Vertical Length</label>
+        <input type="range" min="-200" max="200" value={Veri} onChange={(e) => setVeri(e.target.value)} />
+        <label>Blur</label>
+        <input type="range" min="0" max="200" value={Blur} onChange={(e) => setBlur(e.target.value)} />
+        <label>Color</label>
+        <input type="Color" value={Color} onChange={(e) => setColor(e.target.value)} />
+        <div class="switch">
+          <label>
+            Outline
+            <input type="checkbox" checked={CheckOn} onChange={()=>setCheckon(!CheckOn)} />
+              <span className="lever"></span>
+              inset
+            </label>
+        </div>
+        </div>
+      <div className="Output">
+      <div className="box" style={{boxshadow:'${checkon ?"inset":""} ${Hori}px ${Veri}px ${blur}px ${Color}' }}>
+        <p>box-shadow:{Hori}px {Veri}px {Blur}px {Color}</p>
+      </div>
     </div>
-  );
+     </div>
+     );
 }
 
 export default App;
+
